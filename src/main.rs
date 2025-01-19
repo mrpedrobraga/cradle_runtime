35164c98b7c370b788ac3dd7fe33d11c975b7690
mrpedrobraga <mrhenribraga@gmail.com>
2025-01-19 02:22:05 -0300

    feat: Testing bitmap mode.

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,24 @@
 
 use agb::input::Button;
 
+fn rgb_to_u16(r: u8, g: u8, b: u8) -> u16 {
+    ((b as u16) << 10) | ((g as u16) << 5) | r as u16
+}
+
 #[agb::entry]
 fn main(mut gba: agb::Gba) -> ! {
     agb::println!("Hello World!");
 
     let mut input = agb::input::ButtonController::new();
+    let mut screen = gba.display.video.bitmap3();
+
+    for y in 0..160 {
+        for x in 0..240 {
+            screen.draw_point(x, y, rgb_to_u16((x % 32) as u8, (y % 32) as u8, 0));
+        }
+    }
 
     loop {
-        if input.is_just_pressed(Button::A) {
-            agb::println!("A!");
-        }
-
         agb::display::busy_wait_for_vblank();
         input.update();
     }
